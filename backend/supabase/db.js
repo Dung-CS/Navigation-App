431ce26656133a3b4cf566260fc7b1abd375456d
Dung-CS <phamngocphuongdung2k7@gmail.com>
2026-06-14 00:02:26 +0700

    Initialize Supabase client

---
+++ backend/supabase/db.js
@@ -0,0 +1,9 @@
+const { createClient } = require('@supabase/supabase-js');
+require('dotenv').config();
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
+);
+
+module.exports = supabase;
